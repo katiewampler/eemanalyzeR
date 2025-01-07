3b0a6e8068f280d3ad4bcd89b295a9033ff713f8
Katie Wampler <katie.wampler@oregonstate.edu>
2025-01-07 12:43:21 -0800

    removed default 'hello world' adding is_eem and is_eemlist functions

---
+++ R/utils.R
@@ -0,0 +1,20 @@
+
+#Functions to check if the objects are the right class (eem/eemlist)
+
+  #' Checks if object is an eem
+  #'
+  #' @param eem an object
+  #' @noRd
+  #' @source This function was directly pulled from \link[staRdom]
+  .is_eem <- function(eem) {
+    ifelse(class(eem) == "eem", TRUE, FALSE)
+  }
+
+  #' Checks if object is an eemlist
+  #'
+  #' @param eem an object
+  #' @noRd
+  #' @source This function was directly pulled from \link[staRdom]
+  .is_eemlist <- function(eem) {
+    ifelse(class(eem) == "eemlist", TRUE, FALSE)
+  }
