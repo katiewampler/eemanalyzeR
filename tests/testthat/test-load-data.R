0597149f20f950cc30730c40f33e9293dd827eac
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-01-27 14:04:45 -0800

    making test for checking eems get pulled correctly

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,4 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+#testing loading of eems
+  test_that("loading eems gives eemlist", {
+    expect_s3_class(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), "eemlist")
+  })
 
+  test_that("all eems are loaded", {
+    expect_length(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), 6)
+  })
+
+  test_that("absorbance data throws warning", {
+    expect_warning(eem_dir_read(system.file("extdata", package = "eemanalyzeR"), skip=NULL), "Unable to import file")
+  })
