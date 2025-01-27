e7e0d3c0c241a4ef82ae12dde73b7f09bb3e6964
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-01-27 14:35:05 -0800

    added additional tests to make sure that the pattern and skip argument work

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,12 +1,21 @@
-#testing loading of eems
-  test_that("loading eems gives eemlist", {
-    expect_s3_class(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), "eemlist")
-  })
+  #testing loading of eems
+    test_that("loading eems gives eemlist", {
+      expect_s3_class(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), "eemlist")
+    })
 
-  test_that("all eems are loaded", {
-    expect_length(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), 6)
-  })
+    test_that("all eems are loaded", {
+      expect_length(eem_dir_read(system.file("extdata", package = "eemanalyzeR")), 6)
+    })
 
-  test_that("absorbance data throws warning", {
-    expect_warning(eem_dir_read(system.file("extdata", package = "eemanalyzeR"), skip=NULL), "Unable to import file")
-  })
+    test_that("absorbance data throws warning", {
+      expect_warning(eem_dir_read(system.file("extdata", package = "eemanalyzeR"), skip=NULL), "Unable to import file")
+    })
+
+    test_that("eems pattern selection loading works", {
+      expect_length(eem_dir_read(system.file("extdata", package = "eemanalyzeR"), pattern = "SEM"), 3)
+    })
+
+    test_that("eems skip exclusion loading works", {
+      expect_length(eem_dir_read(system.file("extdata", package = "eemanalyzeR"), skip = "SEM|ABS"), 3)
+    })
+
