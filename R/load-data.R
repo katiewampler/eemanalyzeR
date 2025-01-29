95840e84273301ca5a729d9b354c4a187355c869
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-01-28 16:04:31 -0800

    added meta_read function
    includes checks in dev-test for meta_read, renamed the manual absorbance file (again)

--- R/load-data.R
+++ R/load-data.R
@@ -242,3 +242,96 @@ abs_dir_read <- function(input_dir, pattern = NULL, skip="SEM|BEM|waterfall", fi
     return(abs)
   }
 
+#' Read metadata file from directory
+#'
+#' Reads a .csv or .xlsx file containing metadata related to absorbance and EEM's data.
+#' Please see \link[eemanalyzeR]{metadata} for the required columns and structure.
+#'
+#' @param input_dir path to folder containing metadata file
+#' @param name name of metadata file. optional if metadata is only xlsx or csv file in input_dir
+#' if not specified function will attempt to load any xlsx or csv file in directory and return an error if there is more than one
+#' @param sheet name of sheet containing metadata. only required if metadata isn't the first sheet
+#' @param validate logical. should metadata structure be checked for potential issues that will cause issues during
+#' further processing? highly recommended to keep as TRUE.
+#'
+#' @importFrom readxl read_excel
+#' @importFrom dplyr mutate across any_of
+#' @importFrom utils read.csv
+#'
+#' @returns a data.frame containing metadata of samples
+#' @export
+#'
+#' @examples
+#' metadata <- meta_read(system.file("extdata", package = "eemanalyzeR"))
+meta_read <- function(input_dir, name=NULL, sheet=NULL, validate=T){
+  stopifnot(dir.exists(input_dir)) #make sure directory exists
+
+  #get name of metadata file
+    #if name isn't specified, it pulls all xlsx/csv files, assuming there will only be one
+      #if name is specified, it pulls that from list of xlsx/csv files
+      if(is.null(name)){
+        meta_file <- list.files(input_dir, pattern="xlsx|csv", full.names = T)
+      }else{
+        meta_file <- grep(name, list.files(input_dir, pattern="xlsx|csv", full.names = T), value=T)
+      }
+
+  #check we have one and only one file to read in
+    if(length(meta_file) > 1){
+      stop("attempted to load more than one file, please use 'name' argument to specify metadata file")
+    }
+    if(length(meta_file) == 0){
+      stop("unable to locate metadata in: ", input_dir, "\nplease ensure metadata is in folder and is a .csv or .xlsx file")
+    }
+
+  #read in metadata
+    if(tools::file_ext(meta_file) == "xlsx"){
+      meta <- readxl::read_excel(meta_file, sheet=sheet)
+    } else{
+      meta <- read.csv(meta_file)
+    }
+
+  #ensure metadata conforms to standards for futher processing
+  if(validate == T){
+    #check required columns are there
+      req_cols <- c("data_identifier", "replicate_no", "integration_time_s","run_type", "RSU_area_1s",
+                    "dilution")
+      missing_cols <- setdiff(req_cols, colnames(meta))
+      if(length(missing_cols) > 0) {
+        stop("metadata (", basename(meta_file), ") is missing required column(s): ", paste(missing_cols, collapse = ", "))
+      }
+
+    #ensure integration time and RSU adjust area (and DOC/dilution are numeric)
+      meta <- meta %>% dplyr::mutate(dplyr::across(dplyr::any_of(c("integration_time_s","RSU_area_1s", "dilution", "DOC_mg_L")), as.numeric))
+
+   #make sure there aren't any duplicated samples
+      unique_ID <- paste(meta$data_identifier, meta$replicate_no, meta$integration_time_s, sep="_")
+      if(any(duplicated(unique_ID))){
+        stop("duplicate samples found with the same data_identifier, replicate_no, and integration time:\n", paste(unique(unique_ID[duplicated(unique_ID)]), sep="\n"), "\nif they are indeed duplicates, please use 'replicate_no' to note this by using 1,2,3...")
+      }
+
+    #ensure RSU_area_1s isn't missing data
+      if(any(is.na(meta$RSU_area_1s) | is.numeric(meta$RSU_area_1s) == F)){
+        stop("metadata (", basename(meta_file), ") missing values for RSU adjust area, \nsee help(eemanalyzeR::metadata) for more info")
+      }
+
+    #check dilution isn't missing or 0, if it is assume it's 1 with a warning
+      if(any(meta$dilution == 0 | is.na(meta$dilution))){
+        meta$dilution[meta$dilution == 0 | is.na(meta$dilution)] <- 1
+        warning("dilutions were missing or set to 0. assuming a value of 1 for 'dilution' which indicates no dilution")
+      }
+
+    #check replicate numbers aren't missing, if it is assume it's 1 with a warning
+      if(any(is.na(meta$replicate_no))){
+        meta$replicate_no[is.na(meta$replicate_no)] <- 1
+        warning("replicate numbers were missing. assuming a value of 1 for 'replicate_no' which indicates no replicates")
+      }
+
+    #ensure run_type is either sampleQ or manual (might not matter anymore??)
+      if(any(grepl("sampleQ|manual", meta$run_type, ignore.case = T)==F)){
+        stop("'run_type' must be either 'sampleQ' or 'manual'")
+      }
+
+  }
+
+  return(meta)
+}
