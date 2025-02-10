3174216fa0751e3981fa86f7cce826192e00a705
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-02-10 14:12:06 -0800

    split load-data into dir-read and abs-read

---
+++ R/abs-read.R
@@ -0,0 +1,91 @@
+
+#' Read a single absorbance file into R
+#'
+#' Will accept absorbance files run via Aqualog exported using the sample Q or manually. If the function tries
+#' to load an EEM's file it will return a warning and a NULL value for absorbance.
+#'
+#' @param file file path to the absorbance file to load
+#' @importFrom stringr str_extract_all
+#' @importFrom tools file_ext
+#'
+#' @return An object of class \code{abs} containing:
+#' \itemize{
+#'  \item file The filename of the absorbance data.
+#'  \item sample The sample name of the absorbance data.
+#'  \item n The number of wavelengths absorbance was measured at.
+#'  \item data A \code{data.frame} with absorbance data.
+#'  \item location Directory of the absorbance data.
+#' }
+#' @export
+#'
+#' @examples
+#' abs_files <- list.files(system.file("extdata", package = "eemanalyzeR"),
+#' full.names=TRUE, pattern="ABS")
+#' abs <- abs_read(abs_files[1])
+
+abs_read <- function(file){
+    #captures warning from trying to rbind eem's data
+
+    .safe_rbind <- function(abs){
+      tryCatch({abs <- do.call(rbind, abs)
+      return(abs)},
+      warning = function(w) {
+        # Check if it's a specific error
+        if (grepl("number of columns of result is not a multiple of vector length", conditionMessage(w))) {
+          warning("Unable to import file: ", file, ".\nPlease use the 'pattern' and 'skip' arguments to ensure only absorbance files are selected.", call. = FALSE)
+          return(NULL)
+        } else {
+          stop("An unexpected error occurred: ", conditionMessage(w), call. = FALSE)}})
+    }
+
+    data <- readLines(file)
+    abs <- stringr::str_extract_all(data, "-?\\d+(?:\\.\\d*)?(?:[eE][+\\-]?\\d+)?")
+    abs <- lapply(abs, as.numeric)
+
+    #if abs files was exported manually, remove header rows and extra columns in row 1
+    if(length(abs[[3]]) == 0){
+      #top row has two extra items
+      abs[[4]] <- abs[[4]][-c(3,5)]
+
+      #remove header items
+      abs <- abs[-c(1:3)]
+
+      #merge into df
+      abs <- do.call(rbind, abs)
+      abs <- abs[,c(1,8)]
+    }else{
+      abs <- .safe_rbind(abs)
+    }
+
+    #give column names and make into df if not skipped
+    if(is.null(abs) == F){
+      colnames(abs) <- c("wavelength", "absorbance")
+      abs <- as.data.frame(abs)
+
+      #thrown an error if the wavelength isn't continuous, suggesting transmittance data was added
+      if(sum(diff(abs$wavelength) > 0) > 0){
+        stop("wavelengths aren't continuous, please ensure transmitance data wasn't included in absorbance file:\n", file)
+      }
+
+      #create into class "abs"
+      obj <- list(file = file,
+                  sample = gsub(paste0("[.]", file_ext(file)), "",basename(file)),
+                  n = nrow(abs),
+                  data = abs,
+                  location =dirname(file)
+      )
+
+      class(obj) <- "abs"
+
+      attr(obj, "is_dilution_corrected") <- FALSE
+      attr(obj, "is_DOC_normalized") <- FALSE
+    }else{
+      obj <- NULL
+    }
+
+
+
+    return(obj)
+  }
+
+
