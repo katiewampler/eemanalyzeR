773088af5b2ab9c375136ec7f6b91132fe9d704c
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-02-11 15:20:40 -0800

    added some tests for utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,10 +1,22 @@
-#test yes or no function
+#test functions with metadata/blanks added
+with_mocked_bindings(
+  .yesorno = function(question,
+                      y_response,
+                      n_response) TRUE,
+ test_that("successfully checks augmenting", {
+   expect_false(all(.meta_added(example_eems)))
+   expect_false(.meta_added(example_eems[[1]]))
 
-  with_mocked_bindings(
-    .yesorno = function(question,
-                        y_response,
-                        n_response) TRUE,
-    test_that("a y returns a positive response", {
-      expect_true(.yesorno("does this work", "yes", "no"))
-    })
-  )
+   expect_false(all(.blk_added(example_eems)))
+   expect_false(.blk_added(example_eems[[1]]))
+
+   eemlist <- add_metadata(metadata, example_eems)
+   expect_true(all(.meta_added(eemlist)))
+   expect_true(.meta_added(eemlist[[1]]))
+
+   eemlist <- add_blanks(eemlist)
+   expect_true(all(.blk_added(eemlist)))
+   expect_true(.blk_added(eemlist[[1]]))
+
+ })
+)
