1f8f333b60ebf58b03237de6a47ea5ca1076a279
Katie Wampler <114772563+katiewampler@users.noreply.github.com>
2025-04-16 14:31:45 -0700

    updating main index function, still need to finish absorbance indices
    did pass checks without any new notes

---
+++ R/eemanalyzeR-indices.R
@@ -0,0 +1,195 @@
+#' Default package methods for fluorescence and absorbance indices
+#'
+#' @importFrom zoo na.spline
+#' @param eemlist an \code{eemlist} object containing EEM's data. See details for more info.
+#' @param abslist an \code{abslist} object containing absorbance data.
+#' @param return either "long" or "wide" to specify the format of the indices data.frames
+#' @note If absorbance is not at a 1 nanometer interval, absorbance will be interpolated using \link[zoo]{na.spline} which fills in missing values
+#' using spline interpolation.
+#'
+#' @return a list with two objects:
+#' \itemize{
+#'   \item eem_index: a data.frame of all the fluorescence indices
+#'   \item abs_index: a data.frame of all the absorbance indices
+#' }
+#' If \code{return} is "long" each data.frame will have four columns:
+#' \itemize{
+#'  \item sample_name: the name of the sample
+#'  \item meta_name: the name of the sample in the metadata if metadata has been added, otherwise the sample name again
+#'  \item metric: the name of the index being reported
+#'  \item value: the value of the index
+#'  \item QAQC_flag: any flags associated with the data. See details for more infomation.
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' abslist <- add_metadata(metadata, example_absorbance)
+#' eemlist <- add_metadata(metadata, example_eems)
+#' indices <- eemanalyzeR_indices(eemlist, abslist)
+eemanalyzeR_indices <- function(eemlist, abslist, return="long"){
+  #get fluoresence peaks
+  #define wavelengths for coble peaks
+  peaks <- list(pB = list(ex=270:280, em=300:320),
+                pT = list(ex=270:280, em=320:350),
+                pA = list(ex=250:260, em=380:480),
+                pM = list(ex=310:320, em=380:420),
+                pC = list(ex=330:350, em=420:480),
+                pD = list(ex=390, em=509),
+                pE = list(ex=455, em=521),
+                pN = list(ex=280, em=370))
+
+  #helper functions to flag data [maybe move to broader function to flag no matter what function used]
+  missing_doc_flag <- function(index){
+    doc_flag <- grepl("SUVA|SVA|DOC", index$metric) & is.na(index$value)
+    index$QAQC_flag[doc_flag] <- "DOC_01"
+    return(index)
+  }
+
+  #helper functions to get fluorescence index functions
+  #only coble peaks are different if DOC normalized or not
+  #interpolates to 1 nm and gives max value in that range
+  peak_max <- function(eem, ex, em){
+    ex_p <- rep(ex, length(em)) #gives values to interpolate between
+    em_p <- rep(em, length(ex)) #gives values to interpolate between
+    int_res <- pracma::interp2(eem$ex, eem$em, eem$x, ex_p, em_p)
+    max_res <- max(int_res, na.rm=T)
+    return(max_res)
+  }
+
+  calc_peaks <- function(eem, doc=FALSE){
+    if(doc){
+      pvals <- sapply(peaks, function(p) peak_max(eem, p$ex, p$em))
+      if(.meta_added(eem)){pvals_norm <- pvals / eem$doc_mgL}else{pvals_norm <- rep(NA, length(pvals))}
+      names(pvals_norm) <- paste0(names(pvals_norm), "_DOCnorm")
+      pvals <- pvals_norm
+    }else{
+      pvals <- sapply(peaks, function(p) peak_max(eem, p$ex, p$em))
+      pvals <- c(pvals,
+                 rAT = unname(pvals["pA"] / pvals["pT"]),
+                 rCA = unname(pvals["pC"] / pvals["pA"]),
+                 rCM = unname(pvals["pC"] / pvals["pM"]),
+                 rCT = unname(pvals["pC"] / pvals["pT"]))
+    }
+
+    return(pvals)
+  }
+
+  calc_FI <- function(eem){
+    f_470 <- pracma::interp2(eem$ex, eem$em, eem$x, 370, 470)
+    f_520 <- pracma::interp2(eem$ex, eem$em, eem$x, 370, 520)
+    FI <- f_470/f_520
+    return(FI)
+  }
+
+  calc_HIX <- function(eem, type="zsolnay"){
+    em_high <- 435:480
+    em_low <- 300:345
+    ex_254 <- rep(254, length(em_low))
+    sum_high <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
+                                    ex_254, em_high))
+    sum_low <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
+                                   ex_254, em_low))
+
+    if(type == "ohno"){HIX <- sum_high/(sum_low + sum_high)}else{HIX <- sum_high/(sum_low)}
+    return(HIX)
+  }
+
+  calc_fresh <- function(eem){
+    f_380 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 380)
+    f_420_435 <- peak_max(eem, 310, 420:435)
+    fresh <- f_380/f_420_435
+    return(fresh)
+  }
+
+  calc_BIX <- function(eem){
+    f_380 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 380)
+    f_430 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 430)
+    BIX <- f_380/f_430
+    return(BIX)
+  }
+
+  #get fluorescence indices
+  eem_index <- do.call("rbind", lapply(eemlist, function(eem){
+    vals <- c(calc_peaks(eem),
+              calc_peaks(eem, doc=TRUE),
+              FI = calc_FI(eem),
+              HIX = calc_HIX(eem),
+              HIX_ohno = calc_HIX(eem, "ohno"),
+              fresh = calc_fresh(eem),
+              BIX = calc_BIX(eem))
+    name_type <- ifelse(.meta_added(eem), "meta_name", "sample")
+    index <- data.frame(sample_name= get_sample_info(eem, "sample"),
+                        meta_name=get_sample_info(eem, name_type),
+                        metric = names(vals),
+                        value = unname(vals))}))
+
+  #add flags and change NA to -9999
+  #TODO: flag potential high or low values, values where reporting just noise
+  eem_index <- missing_doc_flag(eem_index)
+  eem_index[is.na(eem_index)] <- "-9999"
+
+  #absorbance peaks
+  #interpolate absorbance data to 1 nm intervals
+  abs_interp <- lapply(abslist, function(x){
+    abs <- data.frame(x$data)
+    abs_filled <- merge(abs, data.frame(X1=min(abs$X1):max(abs$X1)), all=T)
+    abs_filled <- zoo::na.spline(abs_filled)
+    x$data <- as.matrix(abs_filled)
+    x$n <- nrow(abs_filled)
+    return(x)
+  })
+
+  #specify SUVA peaks
+  suva_wl <- list(SUVA254 = 254, SUVA280=280, SUVA350=350,
+                  SUVA370 = 370, SVA412 = 412, SVA440 = 440,
+                  SVA510 = 510, SVA532=532, SVA555=555)
+
+  #get SVA and SUVA values, if no DOC, return NA
+  calc_suva <- function(abs){
+    suva_metrics <- sapply(suva_wl, function(wl){
+      if(.meta_added(abs)){
+        abs_val <- unname(abs$data[abs$data[,1] == wl,2]) /abs$doc_mgL
+      }else{
+        abs_val <- NA
+      }})
+    return(suva_metrics)}
+
+
+  abs_index <- do.call("rbind", lapply(abs_interp, function(abs){
+    vals <- c(calc_suva(abs))
+    name_type <- ifelse(.meta_added(abs), "meta_name", "sample")
+    index <- data.frame(sample_name= get_sample_info(abs, "sample"),
+                        meta_name=get_sample_info(abs, name_type),
+                        metric = names(vals),
+                        value = unname(vals))}))
+
+  abs_index <- missing_doc_flag(abs_index)
+
+  #get spectral ratios
+  #TODO modify this code still
+  #S275_295 <- sapply(lapply(data[,-ncol(data)]*log(10)*100/cuvle, staRdom:::abs_fit_slope,
+   #                         wl=data$wavelength, lim=c(275, 295),
+    #                        l_ref=275), function(res) res$coefficients)
+
+ # S350_400 <- sapply(lapply(data[,-ncol(data)]*log(10)*100/cuvle, staRdom:::abs_fit_slope,
+  #                          wl=data$wavelength, lim=c(350, 400),
+   #                         l_ref=350), function(res) res$coefficients)
+
+
+ # abs_out$S275_295 <- as.numeric(S275_295)
+  #abs_out$S350_400 <- as.numeric(S350_400)
+
+#  abs_out$SR <- abs_out$S275_295 / abs_out$S350_400
+
+
+  #TODO: run functions across all absorbance list items
+
+  #flag as needed for ratios with low noise
+  #TODO: add flags
+
+
+  #return list of index values
+  index <- list(eem_index = eem_index, abs_index=abs_index)
+
+}
--- R/get-indices.R
+++ R/get-indices.R
@@ -21,173 +21,9 @@ get_indices_function <- function(index_method="eemanalyzeR"){
          stop(index_method, " is not a known function to generate indices\n  to create your own see vingette browseVingettes('eemanalyzeR')"))
 }
 
-#interpolates to 1 nm and gives max value in that range
-peak_max <- function(eem, ex, em){
-  ex_p <- rep(ex, length(em)) #gives values to interpolate between
-  em_p <- rep(em, length(ex)) #gives values to interpolate between
-  int_res <- pracma::interp2(eem$ex, eem$em, eem$x, ex_p, em_p)
-  max_res <- max(int_res, na.rm=T)
-  return(max_res)
-}
 
 
-#' Default package methods for indices
-#'
-#' @importFrom zoo na.spline
-#' @param eemlist an \code{eemlist} object containing EEM's data. See details for more info.
-#' @param abslist an \code{abslist} object containing absorbance data.
-#' @note If absorbance is not at a 1 nanometer interval, absorbance will be interpolated using \link[zoo]{na.spline} which fills in missing values
-#' using spline interpolation.
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' abslist <- add_metadata(metadata, abslist)
-#' eemlist <- add_metadata(metadata, eemlist)
-eemanalyzeR_indices <- function(eemlist, abslist){
-  #TODO: warn if processing steps haven't been done
 
-  #get fluoresence peaks
-    #list out the wavelengths for coble peaks
-    peaks <- list(pB = list(ex=270:280, em=300:320),
-                  pT = list(ex=270:280, em=320:350),
-                  pA = list(ex=250:260, em=380:480),
-                  pM = list(ex=310:320, em=380:420),
-                  pC = list(ex=330:350, em=420:480),
-                  pD = list(ex=390, em=509),
-                  pE = list(ex=455, em=521),
-                  pN = list(ex=280, em=370))
-
-    #loop across list of peaks and list of eem samples to get coble peak values
-    #TODO: also return DOC normalized values here??
-    eem_index <- sapply(eemlist, function(eem){
-      coble <- sapply(peaks, function(x){
-        peak_val <- peak_max(eem, x$ex, x$em)
-        return(peak_val)
-      })
-      return(coble)
-  }, simplify = F)
-
-    #get ratios from the peak values
-    eem_index <- sapply(eem_index, function(x){
-      ratio <- c(x, rAT = unname(x["pA"] / x["pT"]),
-                  rCA = unname(x["pC"] / x["pA"]),
-                  rCM = unname(x["pC"] / x["pM"]),
-                  rCT = unname(x["pC"] / x["pT"]))
-      return(ratio)
-    })
-
-    #get other fluorescence peaks
-    fi <- sapply(eemlist, function(eem){
-      f_470 <- pracma::interp2(eem$ex, eem$em, eem$x, 370, 470)
-      f_520 <- pracma::interp2(eem$ex, eem$em, eem$x, 370, 520)
-      FI <- f_470/f_520
-      return(FI)
-    })
-    hix <- sapply(eemlist, function(eem){
-      em_435_480 <- seq(from = 435, to = 480, by = 1)
-      em_300_345 <- seq(from = 300, to = 345, by = 1)
-      ex_254 <- rep(254, length(em_300_345))
-      sum_em_435_480 <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
-                                            ex_254, em_435_480))
-      sum_em_300_345 <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
-                                            ex_254, em_300_345))
-      HIX <- sum_em_435_480/(sum_em_300_345)
-
-    })
-    hix_o <- sapply(eemlist, function(eem){
-      em_435_480 <- seq(from = 435, to = 480, by = 1)
-      em_300_345 <- seq(from = 300, to = 345, by = 1)
-      ex_254 <- rep(254, length(em_300_345))
-      sum_em_435_480 <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
-                                            ex_254, em_435_480))
-      sum_em_300_345 <- sum(pracma::interp2(eem$ex, eem$em, eem$x,
-                                            ex_254, em_300_345))
-      HIX_o <- sum_em_435_480/(sum_em_300_345 + sum_em_435_480)
-
-    })
-    fresh <- sapply(eemlist, function(eem){
-      f_380 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 380)
-      f_420_435 <- peak_max(eem, 310, 420:435)
-        fresh <- f_380/f_420_435
-    })
-    bix <- sapply(eemlist, function(eem){
-      f_380 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 380)
-      f_430 <- pracma::interp2(eem$ex, eem$em, eem$x, 310, 430)
-      BIX <- f_380/f_430
-    })
-
-    #combine all index values together
-    eem_index <- rbind(eem_index, FI = fi, HIX=hix, HIX_ohno=hix_o,
-                   fresh=fresh, BIX=bix)
-
-    #give names to columns
-    if(all(sapply(eemlist, .meta_added))){
-      colnames(eem_index) <- get_sample_info(eemlist, "meta_name")
-    }else{
-      colnames(eem_index) <- get_sample_info(eemlist, "sample")
-    }
-
-  #absorbance peaks
-    #interpolate absorbance data to 1 nm intervals
-    abs_interp <- lapply(abslist, function(x){
-      abs <- data.frame(x$data)
-      abs_filled <- merge(abs, data.frame(X1=min(abs$X1):max(abs$X1)), all=T)
-      abs_filled <- zoo::na.spline(abs_filled)
-      x$data <- as.matrix(abs_filled)
-      x$n <- nrow(abs_filled)
-      return(x)
-    })
-
-    #get SVA and SUVA values if DOC is added
-      if(any(sapply(abslist, .meta_added))){
-
-        suva <- list(SUVA254 = 254, SUVA280=280, SUVA350=350,
-                     SUVA370 = 370, SVA412 = 412, SVA440 = 440,
-                     SVA510 = 510, SVA532=532, SVA555=555)
-
-        abs_index <- sapply(abs_interp, function(abs, doc){
-          suva_metrics <- sapply(suva, function(x){
-            abs_val <- unname(abs$data[abs$data[,1] == x,2]) /abs$doc_mgL
-              return(abs_val)
-          })})
-
-        #TODO: add flag for missing DOC data
-
-      }
-
-    #get spectral ratios
-    #TODO modify this code still
-    S275_295 <- sapply(lapply(data[,-ncol(data)]*log(10)*100/cuvle, staRdom:::abs_fit_slope,
-                              wl=data$wavelength, lim=c(275, 295),
-                              l_ref=275), function(res) res$coefficients)
-
-    S350_400 <- sapply(lapply(data[,-ncol(data)]*log(10)*100/cuvle, staRdom:::abs_fit_slope,
-                              wl=data$wavelength, lim=c(350, 400),
-                              l_ref=350), function(res) res$coefficients)
-
-
-    abs_out$S275_295 <- as.numeric(S275_295)
-    abs_out$S350_400 <- as.numeric(S350_400)
-
-    abs_out$SR <- abs_out$S275_295 / abs_out$S350_400
-
-    #give names to columns
-    if(all(sapply(abslist, .meta_added))){
-      colnames(abs_index) <- get_sample_info(abslist, "meta_name")
-    }else{
-      colnames(abs_index) <- get_sample_info(abslist, "sample")
-    }
-
-    #flag as needed for ratios with low noise
-    #TODO: add flags
-
-
-    #return list of index values
-    index <- list(eem_index = eem_index, abs_index=abs_index)
-
-  }
 
 eemR_indices <- function(){
   print("eemR indices")
@@ -204,9 +40,10 @@ usgs_indices <- function(){
 #' @param index_method currently supports "eemanalyzeR", "eemR", and "usgs". See \link[eemanalyzeR]{get_indices_function} for more information.
 
 #' @return a data.frame with index values for each sample with a row for each sample
-#' @export
 #'
 get_indices <- function(eemlist, abslist, index_method){
+  #TODO: warn if processing steps haven't been done
+
 
   #get function to get indices
   index_function <- get_indices_function(index_method)
