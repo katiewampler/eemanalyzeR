ba977a514b46a4d108d8142eba640a51013076e9
ryanpcole <69218688+ryanpcole@users.noreply.github.com>
2025-11-12 14:58:05 -0800

    cut this branch down to part two of run_eems flowchart

--- tests/testthat/test-meta-read.R
+++
@@ -1,9 +0,0 @@
-#testing loading metadata
-test_that("metadata loads", {
-  # Test for loading with only directory as input
-  expect_s3_class(suppressMessages(meta_read(system.file("extdata", package = "eemanalyzeR"))), "data.frame")
-  # Test for loading given full file path
-  expect_s3_class(suppressMessages(meta_read(system.file("extdata/metadata_example.csv", package = "eemanalyzeR"))), "data.frame")
-  # Should error if given the wrong name
-  expect_error(suppressMessages(meta_read(system.file("extdata/badexample.csv", package = "eemanalyzeR"))), "unable to locate metadata")
-})
