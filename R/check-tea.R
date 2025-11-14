6a70a8e3d76991e15e14c32823096687b981ff8e
katiewampler <katie.wampler@oregonstate.edu>
2025-11-14 09:25:33 -0800

    created check tea std function

--- R/check-tea.R
+++ R/check-tea.R
@@ -3,7 +3,8 @@
 #' Calculate the indices in a tea standard to ensure they are consistent
 #' with the long-term standard.
 #'
-#' @param x an object of class `eem`, `eemlist`, `abs`, or `abslist`, needs to be fully processed
+#' @param eemlist an object of class `eemlist`, needs to be fully processed
+#' @param abslist an object of class `abslist`, needs to be fully processed
 #' @param eem_std a `eem` object containing the long-term average tea standards for fluorescence
 #' @param abs_std an `abs` object containing the long-term average tea standards for absorbance
 #' @param tolerance what is the maximum percentage the tea standard can vary from the long-term values without being flagged?
@@ -19,20 +20,31 @@
 #'
 #' If `vals` is TRUE:
 #'  - a `data.frame` with the index, the observed value, the long-term standard value, and the percent deviation
+#'
+#' @export
+#' @examples
+#' #get tea standards
+#' eem_std <- readRDS(file.path(system.file("extdata", package = "eemanalyzeR"),
+#' "eem-tea-std.rds"))
+#' abs_std <- readRDS(file.path(system.file("extdata", package = "eemanalyzeR"),
+#' "abs-tea-std.rds"))
+#'
+#' check_tea_std(example_processed_eems, example_processed_abs,
+#'               eem_std, abs_std)
 
-check_tea_std <- function(x, eem_std, abs_std, tolerance=0.2, index_method="eemanalyzeR", vals = FALSE){
+check_tea_std <- function(eemlist, abslist, eem_std, abs_std, tolerance=0.2,
+                          index_method="eemanalyzeR", vals = FALSE){
+
   stopifnot(.is_eem(eem_std), .is_abs(abs_std), is.numeric(tolerance),
-            .is_eem(x)|.is_eemlist(x)|.is_abs(x)|.is_abslist(x))
+            .is_eemlist(eemlist)|.is_abslist(abslist))
 
-  #get x attributes to make sure they're processed the same as the standard
-    x_att <- attributes(x)
-    x_att$names <- NULL
+  #get attributes to make sure they're processed the same as the standard
+    if(any(check_processing(eemlist)$done != check_processing(eem_std)$done)){
+      stop("processing steps are different between tea standard and eemlist")
+    }
 
-    if(.is_eem(x)){std_att <- attributes(eem_std)}
-    if(.is_abs(x)){std_att <- attributes(abs_std)}
-
-    std_att$names <- NULL
-    stopifnot(setequal(std_att, x_att))
+  #get index function
+    index_function <- get_indices_function(index_method)
 
   #calculate indices for standard
     #make std a length 1 eemlist/abslist
@@ -41,18 +53,38 @@ check_tea_std <- function(x, eem_std, abs_std, tolerance=0.2, index_method="eema
 
     abs_std <- list(abs_std)
     class(abs_std) <- "abslist"
-    index_function <- get_indices_function(index_method)
 
     std_index <- index_function(eem_std, abs_std)
 
-  #calculate indices for tea (in progress)
-    #x_tea <- subset_qaqc(x, type="tea_std")
-   # x_index <- index_function(x_tea)
+  #calculate indices for tea
+    tea_index <- index_function(subset_qaqc(eemlist, type="tea_std"), subset_qaqc(abslist, type="tea_std"))
+
+  #tidy and combine indices
+    abs_ind <- merge(tea_index$abs_index, std_index$abs_index %>% select(.data$index, .data$value) %>%
+                       dplyr::rename(std_val = .data$value), by="index") %>% mutate(type = "abs")
+
+    eem_ind <- merge(tea_index$eem_index, std_index$eem_index %>% select(.data$index, .data$value) %>%
+                       dplyr::rename(std_val = .data$value), by="index") %>% mutate(type="eem")
+
+    indices <- rbind(eem_ind, abs_ind)
 
   #determine how many are within threshold
+    #get non flagged values
+    indices <- indices[!(grepl("[A-Z]{3,}[0-9]{1,}", indices$value) |  grepl("[A-Z]{3,}[0-9]{1,}", indices$std_val)),]
+
+    #calc percent different
+    indices$per_dif <- abs(as.numeric(indices$std_val) - as.numeric(indices$value)) / as.numeric(indices$std_val)
 
   #calculate percentage that are outside threshold (per sample??) -> mark tea only??? -> similar to blanks
+    report <- indices %>% mutate(tea_flag = ifelse(.data$per_dif > tolerance, TRUE, FALSE)) %>%
+      dplyr::group_by(.data$meta_name, .data$type) %>% dplyr::summarise(tea_flag = any(.data$tea_flag),
+                                                                per_out = sum(.data$tea_flag)/ dplyr::n())
 
+    if(vals){
+      return(indices)
+    }
+
+    return(report)
   #in outer function ->
     #print to readme (add a spot for tea check), if not checked mark that too
     #note to flag indices if readme isn't NULL/not checked
