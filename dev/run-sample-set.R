b73aa68755706a23c62a05d8340452ff2e940791
katiewampler <katie.wampler@oregonstate.edu>
2025-11-24 10:38:43 -0800

    updating code to run real samples

--- dev/run-sample-set.R
+++ dev/run-sample-set.R
@@ -1,5 +1,7 @@
-dir <- "C:/Users/wampleka/Downloads/2025_11_14"
-save <- ""
+date <- "2025_11_14"
+dir <- "T:/Research/Wildfire_Water_Security/02_Nodes/01_Empirical/05_Projects/storm-chasing/Flat-Fire/Data/water-quality/DOM-EEMs"
+input_dir <- file.path(dir, date)
+
   abs <- abs_dir_read(input_dir)
   eems <- eem_dir_read(input_dir)
   meta <- meta_read(input_dir)
@@ -10,9 +12,10 @@ save <- ""
   #processing steps
   abs <- correct_dilution(abs)
   eems <- process_eem(eems, abs, width=c(16,3,20,15))
-  plot_eem(eems, remove_lower = T)
+  eem_plots <- plot(eems, remove_lower = T)
+  abs_plots <- plot(abs)
   indices <- get_indices(eems, abs, return="wide")
   abs_index <- indices$abs_index
   eem_index <- indices$eem_index
 
-  export_data(eems, abs, "devtest5", tempdir(),meta, indices, plots)
+  export_data(eems, abs, date, file.path(dir, date), meta, indices, eem_plots, abs_plots)
